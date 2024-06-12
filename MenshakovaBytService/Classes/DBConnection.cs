40231819886f3a9231bf6d3dcfc004680bfb5166
Lyubov Menshakova <menshakova222@gmail.com>
2024-06-12 15:08:30 +0300

    Установлено соединение с базой данных + добавлена модель базы данных (edmx)

---
+++ MenshakovaBytService/Classes/DBConnection.cs
@@ -0,0 +1,14 @@
+﻿using MenshakovaBytService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenshakovaBytService.Classes
+{
+    public static class DBConnection
+    {
+        public static MenshakovaDBBytServiceEntities DBConnect = new MenshakovaDBBytServiceEntities();
+    }
+}
